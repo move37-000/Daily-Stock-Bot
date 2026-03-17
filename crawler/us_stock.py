8d155fd707e2c41d4823d9c3cd54c8091c793350
move37-000 <move37.000@gmail.com>
2026-03-17 21:06:58 +0900

    feat: 미국 주식 최신 뉴스 수집 기능 추가

--- crawler/us_stock.py
+++ crawler/us_stock.py
@@ -18,6 +18,15 @@ def fetch_us_stocks(tickers):
         change = close - prev['Close']
         change_pct = (change / prev['Close']) * 100
 
+        # 뉴스 가져오기 (최신 3개)
+        news = []
+        for item in ticker.news[:3]:
+            news.append({
+                'title': item.get('title', ''),
+                'link': item.get('link', ''),
+                'publisher': item.get('publisher', '')
+            })
+
         results.append({
             'symbol': symbol,
             'close': close,
